016196021fad1601522c40a2a96b3b9c44b67b97
Vitaly Kalinkin <lattyf@gmail.com>
2009-07-08 16:03:09 +0500

    Решил задачу 086

--- Problem_086/Program.fs
+++ Problem_086/Program.fs
@@ -1,7 +1,5 @@
 ﻿open System
 
-let result = 0
-
 // Максимальное возможное количество решений.
 let maxCount = 1000000
 
@@ -14,17 +12,45 @@ let GetShortestPath (a:int) (b:int) (c:int) =
     let sum = b + c
     let sq = a * a + sum * sum
     let value = Math.Sqrt((float)sq)
-    if (IsInteger value) then (int)value else (-1:int)
+    value
+    
+// Минимальное значение в листе.
+let rec Min x =
+    match x with
+        | [] -> invalid_arg "x is empty"
+        | head::[] -> head
+        | head::tail -> if head < (Min tail) then head else (Min tail)
     
 // Количество целочисленных путей для конкретного M
 let GetAnswerForM (M:int) = 
-    1
+    //printfn "Finding paths at size %A" M
+    let mutable result:int = 0
+    for a in 1..M do
+        for b in a..M do
+            let mutable results = []
+            
+            results <- ((GetShortestPath a b M) :: results)
+                
+            if a <> b then
+                results <- ((GetShortestPath b a M) :: results)
+                
+            if a <> M then
+                results <- ((GetShortestPath M b a) :: results)
+                
+            let minValue = Min results
+            
+            if IsInteger minValue then
+                //printfn "Path %A %A %A is %A" a b M minValue
+                result <- result + 1
+                            
+    result
     
 let mutable count = 0
 let mutable currentM = 1;
 
-while count < maxCount do
+while count <= maxCount do
     count <- count + GetAnswerForM currentM
+    printfn "Current count with M = %A : %A" currentM count
     currentM <- currentM + 1;
-       
-printfn "Answer: %A" count
+           
+printfn "For M = %A answer: %A" (currentM - 1) count
