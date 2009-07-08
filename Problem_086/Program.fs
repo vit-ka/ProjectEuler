f6669cf5e15626de8d602eb8add631910d9e0f9b
Vitaly Kalinkin <lattyf@gmail.com>
2009-07-08 13:03:49 +0500

    Добавил еще почти что расчет ответа.

--- Problem_086/Program.fs
+++ Problem_086/Program.fs
@@ -2,8 +2,8 @@
 
 let result = 0
 
-// Максимальное возможная длина пути.
-let maxNumber = 1000000
+// Максимальное возможное количество решений.
+let maxCount = 1000000
 
 // Является ли число целым.
 let IsInteger (a:float) =
@@ -15,5 +15,16 @@ let GetShortestPath (a:int) (b:int) (c:int) =
     let sq = a * a + sum * sum
     let value = Math.Sqrt((float)sq)
     if (IsInteger value) then (int)value else (-1:int)
+    
+// Количество целочисленных путей для конкретного M
+let GetAnswerForM (M:int) = 
+    1
+    
+let mutable count = 0
+let mutable currentM = 1;
+
+while count < maxCount do
+    count <- count + GetAnswerForM currentM
+    currentM <- currentM + 1;
        
-printfn "Answer: %A" (GetShortestPath 6 5 3)
+printfn "Answer: %A" count
