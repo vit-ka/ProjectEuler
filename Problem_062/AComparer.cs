6bde8dc34a67d7a810e23b23c9984c2f0fee5f40
Vitaly Kalinkin <vitaly.kalinkin@gmail.com>
2010-12-18 15:19:34 +0300

    Problem 062

---
+++ Problem_062/AComparer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_062
+{
+    internal class AComparer : IComparer<IList<byte>>, IEqualityComparer<IList<byte>>
+    {
+        public int Compare(IList<byte> x, IList<byte> y)
+        {
+            if (x.Count != y.Count)
+                return x.Count - y.Count;
+
+            for (int i = 0; i < x.Count; ++i)
+                if (x[i] != y[i])
+                    return x[i] - y[i];
+
+            return 0;
+        }
+
+        public bool Equals(IList<byte> x, IList<byte> y)
+        {
+            return Compare(x, y) == 0;
+        }
+
+        public int GetHashCode(IList<byte> obj)
+        {
+            return obj.Sum(b => b*11);
+        }
+    }
+}
---
+++ Problem_062/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_062
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime d = DateTime.Now;
+            IEnumerable<long> cubes = GenerateCubes();
+
+            IEnumerable<IList<byte>> splittedCubes = SplitCubes(cubes);
+
+            var groupedCubes =
+                splittedCubes.GroupBy(digits => digits, new AComparer()).Where(
+                    groupedDigits => groupedDigits.Count() == 5).OrderBy(groupedDigits => groupedDigits.Count()).Select(
+                        groupedDigits => groupedDigits).First();
+
+            var en = groupedCubes.GetEnumerator();
+            en.MoveNext();
+            var seq = en.Current;
+
+            var comp = new AComparer();
+            var index = splittedCubes.TakeWhile(a => !comp.Equals(a, seq)).Count();
+
+            Console.WriteLine(cubes.ToArray()[index]);
+            Console.WriteLine("Time: " + (DateTime.Now - d).TotalMilliseconds);
+        }
+
+        private static IEnumerable<IList<byte>> SplitCubes(IEnumerable<long> cubes)
+        {
+            return cubes.Select(cube => SplitCube(cube));
+        }
+
+        private static IList<byte> SplitCube(long cube)
+        {
+            var result = new List<byte>();
+
+            while (cube > 0)
+            {
+                result.Add((byte) (cube % 10));
+                cube = cube/10;
+            }
+
+            result.Sort();
+            return result;
+        }
+
+        private static IEnumerable<long> GenerateCubes()
+        {
+            long index = 1;
+
+            while (index * index * index < 1000000000000L)
+            {
+                yield return index*index*index;
+                index++;
+            }
+        }
+    }
+}
